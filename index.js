38250ada7bd29a4cc0b836b7f40d3a30f10f8d00
jankuca <jan@jankuca.com>
2014-12-13 22:28:11 +0100

    add MockTwitter, change exports

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-module.exports = require('./lib/twitter');
+
+var Twitter = require('./lib/twitter');
+
+
+module.exports = Twitter;
+module.exports.OAuth = require('./lib/oauth');
+module.exports.Twitter = Twitter;
+
+
+module.exports.mocks = {
+  MockTwitter: require('./lib/mocks/mock-twitter')
+};
